561564ca785b275801903ec57536cbbf2aa52f9d
Jeffery Lofoneh Asamani <jefferyasamani7@gmail.com>
2024-12-26 08:31:29 +0000

    more organized code

--- main.go
+++ main.go
@@ -1,51 +1,33 @@
 package main
 
 import (
+	"booking-application/helper"
 	"fmt"
 	"strings"
 )
 
-func main()  {
-	conferenceName := "Phanerosis conference"
-	const conferenceTickets = 50
-	var remainingTickets uint=50
+const conferenceTickets = 50
 
-	greetUsers(conferenceName, conferenceTickets, remainingTickets)
+var conferenceName = "Phanerosis conference"
+var remainingTickets uint = 50
+var bookings = []string{}
 
-	bookings :=[]string{}
-	
+func main() {
+
+	greetUsers()
+
 	for {
 
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
-		//ask user for their name
-		fmt.Println("Enter your first name: ")
-		fmt.Scan(&firstName)
+		firstName, lastName, email, userTickets := getUserInput()
 
-		fmt.Println("Enter your last name: ")
-		fmt.Scan(&lastName)
-
-		fmt.Println("Enter your email address:")
-		fmt.Scan(&email)
-
-		fmt.Println("Enter number of tickets: ")
-		fmt.Scan(&userTickets)
-
-		isValidName := len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+		isValidName, isValidEmail, isValidTicketNumber := helper.ValidateUserInput(firstName, lastName, email, userTickets, remainingTickets)
 
 		if isValidEmail && isValidName && isValidTicketNumber {
-			remainingTickets= remainingTickets - userTickets
-			bookings= append(bookings,firstName + " " + lastName)
-			
-			fmt.Printf("Thank you %v %v for booking %v tickets.\nYou will receive a confirmation email at %v shortly.\n", firstName, lastName, userTickets,email)
-			fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
+			bookTicket(userTickets, firstName, lastName, email)
 
-			printFirstNames(bookings)
-			
+			firstNames := getFirstNames()
+			fmt.Printf("The first names of bookings: %v\n", firstNames)
+
 			if remainingTickets == 0 {
 				fmt.Println("Our conference is booked out. Come back next year.")
 				break
@@ -60,25 +42,54 @@ func main()  {
 			if !isValidTicketNumber {
 				fmt.Println("number is tickets you entered is invalid")
 			}
-		} 
+		}
 	}
 }
 
-func greetUsers(confName string, confTickets int, remainingTicket uint) {
-	fmt.Printf("Welcome to %v booking application\n", confName)
-	fmt.Printf("We have a total of %v tickets and %v are still available.\n", confTickets, remainingTicket)
+func greetUsers() {
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
 
 }
 
-func printFirstNames (bookings []string) {
+func getFirstNames() []string {
 	firstNames := []string{}
-			for _, booking := range bookings {
-				names:=strings.Fields(booking)
-				firstNames = append(firstNames, names[0])
-			}
-			fmt.Printf("The first names of bookings: %v\n", firstNames)
-
+	for _, booking := range bookings {
+		names := strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames
 }
-/* 1. Add functionality to tell if the input is invalid after hitting enter and allow the user to enter again. 
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	//ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets.\nYou will receive a confirmation email at %v shortly.\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
+}
+
+/* 1. Add functionality to tell if the input is invalid after hitting enter and allow the user to enter again.
 2. the program to exit after 3 attempts. */
